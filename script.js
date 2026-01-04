d9cd7c4385b02f2ba0ed1b854addb793f9d1095d
Michael50234 <michael.shi7m@gmail.com>
2026-01-04 15:04:00 -0500

    Fixed issues with video and added functions to turn pokemon data into data for questions

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //Question Data
-
 let questionNumber = 0;
 
 const pokemonList = [
@@ -14,8 +13,6 @@ const pokemonList = [
   "Ekans", "Arbok",
   "Pikachu", "Raichu",
   "Sandshrew", "Sandslash",
-  "Nidoran♀", "Nidorina", "Nidoqueen",
-  "Nidoran♂", "Nidorino", "Nidoking",
   "Clefairy", "Clefable",
   "Vulpix", "Ninetales",
   "Jigglypuff", "Wigglytuff",
@@ -37,12 +34,23 @@ const pokemonList = [
   "Ponyta", "Rapidash",
   "Slowpoke", "Slowbro",
   "Magnemite", "Magneton",
-  "Farfetch'd", "Doduo", "Dodrio",
-  "Seel", "Dewgong"
+  "Farfetchd",
+  "Doduo", "Dodrio",
+  "Seel", "Dewgong",
+  "Grimer", "Muk",
+  "Shellder", "Cloyster",
+  "Gastly", "Haunter", "Gengar",
+  "Onix",
+  "Drowzee", "Hypno",
+  "Krabby", "Kingler",
+  "Voltorb", "Electrode",
+  "Exeggcute", "Exeggutor",
+  "Cubone", "Marowak",
+  "Hitmonlee"
 ];
 
-let pokemonData = [];
 
+let pokemonData = [];
 let questionData = [];
 
 
@@ -52,19 +60,18 @@ let start = 95;
 let end = 114;
 
 // User Data
-
-answers = [];
-results = [];
+let answers = [];
+let results = [];
 
 //Generates a random number from 1 to 100
 function randomNumber(){
-    return Math.floor(Math.random() * 100) + 1;
+    return Math.floor(Math.random() * 99) + 1;
 }
 
 // Gets pokemon data from the pokemon API
-async function retriveQuestions(){
+async function retriveData(){
     let poke_nums = [];
-    for(let i = 1; i < 10; i++){
+    for(let i = 0; i < 10; i++){
         poke_nums.push(randomNumber());
     }
 
@@ -73,7 +80,7 @@ async function retriveQuestions(){
             const url = `https://pokeapi.co/api/v2/pokemon/${pokemonList[index].toLowerCase()}`;
             let response = await fetch(url);
             if(!response.ok){
-                throw new error(`Response status: ${response.status}`);
+                throw new Error(`Response status: ${response.status}`);
             }
 
             response = await response.json();
@@ -89,31 +96,43 @@ async function retriveQuestions(){
 }
 
 // Formats pokemon data into data for questions
+function Pokemon(pokemon_dict){
+    this.name = pokemon_dict.name;
+    this.image = pokemon_dict.sprites.back_default;
+    this.height = pokemon_dict.height;
+    this.weight = pokemon_dict.weight;
+}
+
+// Turns the data into data for questions
 function createQuestionData() {
+    pokemonData.forEach((pokemon_dict) => {
+        questionData.push(new Pokemon(pokemon_dict))
+    })
+}
+
+function loadQuestion(){
+    
+}
+
+function loadResult(){
+
+}
+
+function loadFinalResult(){
 
 }
 
 
-function render(){
-    return
-}
 
-retriveQuestions()
-
-
-
-//Event Listeners
+//Event Listeners Welcome Page
 bgVideo.addEventListener('loadedmetadata', () => {
     bgVideo.currentTime = start;
-    bgVideo.play();
+    console.log("changed start time");
 })
 
 bgVideo.addEventListener('timeupdate', () => {
     if (bgVideo.currentTime >= end) {
         bgVideo.currentTime = start;
+        console.log("new loop");
     }
 })
-
-bgVideo.addEventListener("play", () => {
-  console.log("started at", bgVideo.currentTime);
-});
